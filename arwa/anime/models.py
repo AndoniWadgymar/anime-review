40bbfea07582df1d65f2975d41bc0cd66253a47b
Andoni Wadgymar <andowii@gmail.com>
2024-01-02 09:48:43 -0600

    Create anime app and model

---
+++ arwa/anime/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+class Anime(models.Model):
+  title = models.CharField(max_length=100)
+  creator = models.CharField(max_length=100, blank=True)
+  rating = models.FloatField(max_value=10.0, min_value=0.0)
+  episodes = models.IntegerField(min_value=0)
+  aired = models.DateField()
+  ended = models.DateField()
+  # genres
